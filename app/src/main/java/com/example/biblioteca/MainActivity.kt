09a22e3860ac3c666b45d8f69c0d5fbebadc70de
Nahuel Palacio <palacionahuel202@gmail.com>
2023-05-19 19:21:30 -0300

    1
    1

--- app/src/main/java/com/example/biblioteca/MainActivity.kt
+++ app/src/main/java/com/example/biblioteca/MainActivity.kt
@@ -4,32 +4,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textViewUsuario: TextView
-    lateinit var buttonSalir: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //xml
-        textViewUsuario = findViewById(R.id.textView_Email)
-        buttonSalir = findViewById(R.id.button_Salir)
-        val usuario = intent.extras!!.getString("usuario")
-
-        textViewUsuario.text = usuario
-        //
-        buttonSalir.setOnClickListener{
-            salir()
-        }
-
     }
 
-    private fun salir(){
-        Firebase.auth.signOut()
-        finish()
-    }
 }
