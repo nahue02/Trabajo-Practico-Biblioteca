c102df9733a3a6e90101cbfd364cc37953377c58
Nahuel Palacio <palacionahuel202@gmail.com>
2023-06-28 16:51:44 -0300

    1
    1

--- app/src/main/java/com/example/biblioteca/authentication/RegistroActivity.kt
+++ app/src/main/java/com/example/biblioteca/authentication/RegistroActivity.kt
@@ -87,41 +87,6 @@ class RegistroActivity : AppCompatActivity() {
     }
 
 
-/*
-    private fun registrarUsuario(nombre: String, email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-
-                    val user = auth.currentUser
-                    if (user != null) {
-                        val usuario = Usuario(user.uid, nombre, email, password)
-                        /*
-                        firestore.collection("usuarios")
-                            .add(usuario)
-                            .addOnSuccessListener { documentReference ->
-                                Log.d(
-                                    TAG,
-                                    "DocumentSnapshot added with ID: ${documentReference.id}"
-                                )
-                            }
-                            .addOnFailureListener { e ->
-                                Log.w(TAG, "Error adding document", e)
-                            }*/
-
-
-                        updateUI(user)
-                    }
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    mostrarMensaje("Error al registrar el usuario")
-                }
-            }
-    }
-*/
     private fun startMainActivity(user: FirebaseUser?) {
         val i = Intent(this, MainActivity::class.java)
         startActivity(i)
