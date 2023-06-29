c17ec46a9dba3734528ef9b5a631784439c32e8b
Nahuel Palacio <palacionahuel202@gmail.com>
2023-06-29 20:38:21 -0300

    creo que termine
    XD

--- app/src/main/java/com/example/biblioteca/ConfiguracionActivity.kt
+++ app/src/main/java/com/example/biblioteca/ConfiguracionActivity.kt
@@ -31,30 +31,68 @@ class ConfiguracionActivity : AppCompatActivity() {
         binding = ActivityConfiguracionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val editTextNombre = binding.editTextNombreConfig
-        val editTextEmail = binding.editTextEmailConfig
-        val editTextContrasena = binding.editTextContrasenaConfig
-        val buttonActualizarDatos = binding.buttonActualizarDatos
-
         auth = Firebase.auth
         db = FirebaseDatabase.getInstance()
 
-        buttonActualizarDatos.setOnClickListener {
+        val editTextNombre = binding.editTextNombreConfig
+        val editTextEmail = binding.editTextEmailConfig
+        val editTextContrasena = binding.editTextContrasenaConfig
+
+        val buttonActualizarNombre = binding.buttonActualizarNombre
+        val buttonActualizarEmail = binding.buttonActualizarEmail
+        val buttonActualizarContrasena = binding.buttonActualizarContrasena
+
+        buttonActualizarNombre.setOnClickListener {
             val user = auth.currentUser
             val userRef = db.reference.child("usuarios").child(user!!.uid)
 
             val nombre = editTextNombre.text.toString()
+
+            if (nombre.isEmpty()) {
+                mostrarMensaje("Ingrese un nombre")
+            } else {
+                val profileUpdatesDatabase = hashMapOf<String, Any>(
+                    "nombre" to nombre,
+                )
+                userRef.updateChildren(profileUpdatesDatabase)
+            }
+        }
+
+        buttonActualizarEmail.setOnClickListener {
+            val user = auth.currentUser
+            val userRef = db.reference.child("usuarios").child(user!!.uid)
+
             val email = editTextEmail.text.toString()
+
+            if (email.isEmpty()) {
+                mostrarMensaje("Ingrese un email")
+            } else {
+                val profileUpdatesDatabase = hashMapOf<String, Any>(
+                    "email" to email,
+                )
+                userRef.updateChildren(profileUpdatesDatabase)
+
+                user.updateEmail(email)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d(TAG, "User email address updated.")
+                        }
+                    }
+            }
+        }
+
+        buttonActualizarContrasena.setOnClickListener {
+            val user = auth.currentUser
+            val userRef = db.reference.child("usuarios").child(user!!.uid)
+
             val contrasena = editTextContrasena.text.toString()
 
-            if (nombre.isEmpty() || email.isEmpty() || contrasena.isEmpty()) {
-                mostrarMensaje("No pueden haber espacios vacios")
+            if (contrasena.isEmpty()) {
+                mostrarMensaje("Ingrese una contraseña")
             } else if (contrasena.length < 6) {
                 mostrarMensaje("La contraseña no puede tener menos de 6 caracteres")
             } else {
                 val profileUpdatesDatabase = hashMapOf<String, Any>(
-                    "nombre" to nombre,
-                    "email" to email,
                     "contrasena" to contrasena
                 )
 
@@ -66,15 +104,6 @@ class ConfiguracionActivity : AppCompatActivity() {
                             Log.d(TAG, "User password updated.")
                         }
                     }
-
-                user.updateEmail(email)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d(TAG, "User email address updated.")
-                        }
-                    }
-
-
             }
         }
 
